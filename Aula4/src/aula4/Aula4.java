770836c44a4bf97f00e557b0ca10ad8d43647ac5
Zittko <daniel.y.saiki@gmail.com>
2026-03-04 19:19:42 -0300

    teste de commit

---
+++ Aula4/src/aula4/Aula4.java
@@ -0,0 +1,13 @@
+package aula4;
+
+import java.util.Scanner;
+
+
+public class Aula4 {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+    
+}
